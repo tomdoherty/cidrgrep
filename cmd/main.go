b8ce385a498e11f2fef7c867b02f114a5f031b72
Tom Doherty <tom@singlesecond.com>
2020-10-10 15:05:49 +0100

    Prefix the filename when handling multiple inputs

--- cmd/main.go
+++ cmd/main.go
@@ -12,17 +12,23 @@ func main() {
 		log.Fatalf("%s <network/cidr> [files..]", os.Args[0])
 	}
 
+	// when parsing multiple files, prefix with the filename
+	var prefix string
+
 	// process files passed on the command line
 	if len(os.Args) > 2 {
 		for _, file := range os.Args[2:] {
+			if len(os.Args) > 3 {
+				prefix = file + ": "
+			}
 			f, err := os.Open(file)
 			if err != nil {
 				log.Fatal(err)
 			}
-			cidrgrep.Filter(f, os.Stdout, os.Args[1])
+			cidrgrep.Filter(f, os.Stdout, os.Args[1], prefix)
 		}
 	} else {
 		// default to reading stdin
-		cidrgrep.Filter(os.Stdin, os.Stdout, os.Args[1])
+		cidrgrep.Filter(os.Stdin, os.Stdout, os.Args[1], prefix)
 	}
 }
