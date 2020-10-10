25d668be595263900bcea08604ce80c707908aae
Tom Doherty <tom@singlesecond.com>
2020-10-10 15:21:24 +0100

    rm Makefile, add docs

--- cidrgrep.go
+++ cidrgrep.go
@@ -9,19 +9,27 @@ import (
 	"regexp"
 )
 
-// Filter filters input for lines containing an IP within cidr
+// IPAddressRegexp is regexp for matching IP address
+var IPAddressRegexp = regexp.MustCompile(`[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}`)
+
+// Filter reads from its input reader, looking for lines containing IP addresses
+// which match the supplied pattern, which is a CIDR network address expression
+// as accepted by net.ParseCIDR. Any matching lines are written to the output
+// writer. All other input is ignored. The input reader is read to completion,
+// or the first read error. A prefix is prefixed to the output for when parsing
+// multiple files
 func Filter(input io.Reader, output io.Writer, cidr string, prefix string) {
 	scanner := bufio.NewScanner(input)
 
+	// IP is not needed here, just the *IPNet
 	_, ipv4Net, err := net.ParseCIDR(cidr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	re := regexp.MustCompile(`[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}`)
 	for scanner.Scan() {
 		line := scanner.Text()
-		for _, match := range re.FindAllString(line, -1) {
+		for _, match := range IPAddressRegexp.FindAllString(line, -1) {
 			if ipv4Net.Contains(net.ParseIP(match)) {
 				fmt.Fprintf(output, "%s%s\n", prefix, line)
 				break
